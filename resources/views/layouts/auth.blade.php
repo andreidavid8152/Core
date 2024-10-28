0dee64c60ea18d5110eadbbd65dc98ae3f1b996d
(Estudiante) Andrei David Flores Espinoza <andrei.flores@udla.edu.ec>
2024-10-28 12:02:54 -0500

    logo implementado

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">CRUD Futbol</a>
+        <div class="container-fluid d-flex align-items-center">
+            <a class="navbar-brand me-auto mt-3 ms-4" href="/">
+                <!-- Logo de NutriCore -->
+                <img src="{{ asset('images/logo.png') }}" alt="NutriCore Logo" class="img-fluid" style="max-height: 50px;">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
     </nav>
+
 
     <div class="container mt-5">
         @yield('content')
@@ -24,4 +30,5 @@
     <!-- Bootstrap JS and dependencies -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
