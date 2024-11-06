7f29b3d82e7c1628cff3ebde2223f151a1238c53
(Estudiante) Andrei David Flores Espinoza <andrei.flores@udla.edu.ec>
2024-11-05 21:00:33 -0500

    Cambio en nombre de ruta a archivo css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/welcome.css') }}" rel="stylesheet">
     <!-- <link href="{{ asset('css/welcome.css') }}" rel="stylesheet"> -->
 </head>
 
