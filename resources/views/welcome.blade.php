dea32cb47816542aa5884f1ce1773efbef35a844
(Estudiante) Andrei David Flores Espinoza <andrei.flores@udla.edu.ec>
2024-11-29 19:24:13 -0500

    Estilos seguros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <!-- <link href="{{ secure_asset('css/welcome.css') }}" rel="stylesheet"> -->
-    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/welcome.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/welcome.css') }}" rel="stylesheet"> -->
 </head>
 
 <body>
